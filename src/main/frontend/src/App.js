ade71ccad2cd441ee1f83ea753d0f5c576982676
Anis <Anis1011@gmail.com>
2021-01-09 12:01:26 -0800

    added Container.js and table  from Antd

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,15 +1,15 @@
 import "./App.css";
-import { getAllStudents } from "./Client";
 import React, { useEffect, useState } from "react";
-import axios from "axios";
-import fetch from "unfetch";
+import { Table } from "antd";
+import Container from "./Container";
+import { getAllStudents } from "./Client";
 function App() {
   const [state, setState] = useState({
     students: [],
   });
 
   useEffect(() => {
-    fetch("/students").then((res) =>
+    getAllStudents().then((res) =>
       res.json().then((students) => {
         setState({
           students,
@@ -20,19 +20,42 @@ function App() {
   console.log(state.students.length);
 
   if (state.students && state.students.length) {
+    const columns = [
+      {
+        title: "Student Id",
+        dataIndex: "studentId",
+        key: "studentId",
+      },
+      {
+        title: "First Name",
+        dataIndex: "firstName",
+        key: "firstName",
+      },
+      {
+        title: "Last Name",
+        dataIndex: "lastName",
+        key: "lastName",
+      },
+      {
+        title: "Email",
+        dataIndex: "email",
+        key: "email",
+      },
+      {
+        title: "Gender",
+        dataIndex: "gender",
+        key: "gender",
+      },
+    ];
     return (
-      <div>
-        {state.students.map((student) => {
-          return (
-            <div>
-              <h2 key={student.studentId}>{student.studentId} </h2>
-              <p>{student.firstName}</p>
-              <p>{student.email}</p>
-              <p>{student.gender}</p>
-            </div>
-          );
-        })}
-      </div>
+      <Container>
+        <Table
+          dataSource={state.students}
+          columns={columns}
+          rowKey={state.studentId}
+          pagination={false}
+        />
+      </Container>
     );
   } else {
     return <h1>Nothing here</h1>;
--- src/main/frontend/src/Client.js
+++ src/main/frontend/src/Client.js
@@ -1,6 +1,5 @@
-import axios from "axios";
 import fetch from "unfetch";
 
 export const getAllStudents = () => {
-  fetch("/students");
+  return fetch("/students");
 };
